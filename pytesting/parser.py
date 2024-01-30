362571a8446f5ad79566dc1149c0b2c6d631ccdd
Ian DesJardins <ian.desjardins@ithaka.org>
2024-01-30 10:49:29 -0500

    feat: Complete student parsing and error handling

--- pytesting/parser.py
+++ pytesting/parser.py
@@ -11,8 +11,21 @@ class StudentsFileNotFoundError(FileNotFoundError):
 
 def parse_students_file(filename: str) -> list[dict]:
     try:
+        students = []
         with open(filename, "r") as f:
             reader = csv.DictReader(f)
-            return list(reader)
+            for row in reader:
+                first_name, last_name = row["name"].split(" ")
+                students.append(
+                    {
+                        "first_name": first_name,
+                        "last_name": last_name,
+                        "age": row["age"],
+                        "institution": row["school"],
+                    }
+                )
+            return students
     except FileNotFoundError as e:
         raise StudentsFileNotFoundError(f"Could not find file {filename}") from e
+    except ValueError as e:
+        raise StudentParseError(f"Could not parse file {filename}") from e
