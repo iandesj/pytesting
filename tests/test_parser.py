2a0a2b7f5ccb83b0706d260062af05ccfcef8dc9
Ian DesJardins <ian.desjardins@ithaka.org>
2024-01-30 10:20:21 -0500

    test: Assert that student dictionary is valid

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,5 +2,10 @@ from pytesting.parser import parse_students_file
 
 
 def test_parse_students_file():
-    result = parse_students_file("students.csv")
-    assert len(result) == 4
+    results = parse_students_file("students.csv")
+    assert len(results) == 4
+
+    for result in results:
+        assert result["name"]
+        assert result["age"]
+        assert result["school"]
